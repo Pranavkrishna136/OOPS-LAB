fe9ba1a788b705d48935749f4fef61dc1c573ec5
Pranavkrishna136 <pranavkrishna@karunya.edu.in>
2025-09-04 09:41:47 +0530

    Add files via upload

---
+++ charachter.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class charachter {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("ENTER THE SIZE:");
+		int size=sc.nextInt();
+		char[] chara=new char[size];
+		char[] dupl=new char[size];
+		System.out.println("enter the elements:");
+		for (int i=0;i<0;i++)
+		{
+			 chara[i]=sc.next();
+			 
+			
+			
+			
+		}
+		for (int i=0;i<0;i++)
+		{
+		
+		}
+		
+
+	}
+
+}
